dd55933feb3e007687a2d1ac0675bbc980961ba0
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2025-07-30 11:14:49 -0600

    REPL - Tests

---
+++ repl_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input string
+		expected []string
+	}{
+		{ 
+			input: " hello world ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+		{
+			input: "Hello!",
+			expected: []string{"hello!"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+
+		if len(c.expected) != len(actual) {
+			t.Errorf("expected and actual slices don't match")
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("expected and actual slices don't match")
+			}
+		}
+
+	}
+
+}
