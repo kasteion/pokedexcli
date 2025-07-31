dcb2e2906e89c94eaa02ad8ccc64fa44ccce8479
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2025-07-31 11:59:33 -0600

    REPL: REPL

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		input := cleanInput(text)
+		fmt.Println("Your command was:", input[0])
+	}
 }
